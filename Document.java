e448d6a4bcde7a6fab69e15ef733b78df6baa9c1
124 <123@gmail.com>
2020-09-21 10:24:16 +0530

    added

---
+++ Document.java
@@ -0,0 +1,37 @@
+public class Document
+{
+	public String name;
+	public String extension;
+	public String content;
+	public int id;
+
+	public void setName(String Name)
+	{
+		name=Name;
+	}
+	public void setContent(String Content)
+	{
+		content=Content;
+	}
+	public void setExtension(String Extension)
+	{
+		extention=Extension;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public String getContent()
+	{
+		return content;
+	}
+	public String getExtension()
+	{
+		return extension;
+	}
+	
+}
+
+	
+
+
